9e4f08c2bb0492f904a280d6d06aeb191e2fb6a1
duxingxia081 <332931496@qq.com>
2018-11-27 18:27:35 +0800

    请求后台登录

--- src/app/class/userinfo.ts
+++ src/app/class/userinfo.ts
@@ -1,12 +1,9 @@
-import {AppService} from '../service/app.service';
 import {LocalStorageService} from '../service/local-storage.service';
 
 export class UserInfo {
     public authorization: string;
 
-    constructor(public appService: AppService,
-                public localStorageService: LocalStorageService) {
+    constructor(public localStorageService: LocalStorageService) {
         this.authorization = this.localStorageService.getStore('authorization');
-        console.log(this.authorization);
     }
 }
