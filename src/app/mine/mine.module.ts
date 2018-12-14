4cec0c572ff6a844e60d1cd53ed0d979c18ccce1
duxingxia081 <332931496@qq.com>
2018-12-14 15:53:12 +0800

    保护子路由

--- src/app/mine/mine.module.ts
+++ src/app/mine/mine.module.ts
@@ -1,4 +1,4 @@
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {MinePage} from './mine.page';
 import {SharedModule} from '../shared/shared.module';
@@ -8,20 +8,25 @@ import {PasswordInfoComponent} from './my-info/password-info/password-info.compo
 import {SafeInfoComponent} from './my-info/safe-info/safe-info.component';
 import {OtherInfoComponent} from './my-info/other-info/other-info.component';
 import {ModifyUserComponent} from './my-info/person-info/modify-user/modify-user.component';
+import {ActivityPage} from '../activity/activity.page';
+import {ActivityListComponent} from '../activity/activity-list/activity-list.component';
+import {AuthGuard} from '../auth/auth.guard';
+
+const routes: Routes = [
+    {
+        path: '',
+        canActivateChild: [AuthGuard],
+        children: [
+            {path: '', component: MinePage},
+            {path: 'my-info', component: MyInfoComponent}
+        ]
+    }
+];
 
 @NgModule({
     imports: [
         SharedModule,
-        RouterModule.forChild([
-            {
-                path: '',
-                component: MinePage,
-            },
-            {
-                path: 'my-info',
-                component: MyInfoComponent
-            }
-        ])
+        RouterModule.forChild(routes)
     ],
     declarations: [MinePage, MyInfoComponent, PersonInfoComponent, PasswordInfoComponent, SafeInfoComponent, OtherInfoComponent, ModifyUserComponent],
     entryComponents: [
